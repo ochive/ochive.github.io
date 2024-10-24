7995bedac3c9d6475765b4b912a857192c183788
小伙子挺不错 <forumlove@163.com>
2024-10-24 14:03:58 +0800

    fix: 移除废弃的element icons代码

--- src/icons/index.ts
+++
@@ -1,8 +0,0 @@
-import * as ElementPlusIconsVue from "@element-plus/icons-vue";
-import { App } from "vue";
-
-export function registerIcons(app: App<Element>) {
-  for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component);
-  }
-}
